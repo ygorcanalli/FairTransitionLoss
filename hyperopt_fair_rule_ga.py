3602c73bb5b0f49f87928746f222a613e3175f73
Ygor Canalli <ygor.canalli@gmail.com>
2022-05-16 21:17:10 -0300

    prejudice remover and meta fair classifier GA results on equalized odds

--- hyperopt_fair_rule_ga.py
+++ hyperopt_fair_rule_ga.py
@@ -2,12 +2,16 @@
 from multiprocessing import Pool
 
 import pandas as pd
+from datetime import datetime
 from aif360.datasets import AdultDataset
 from aif360.metrics import ClassificationMetric
-from models import FairMLP
-from aif360.algorithms.inprocessing import PrejudiceRemover, AdversarialDebiasing, MetaFairClassifier
-from fitness_rules import paraboloid_parity, linear_parity
+from models import FairMLP, SimpleMLP
+from aif360.algorithms.inprocessing import PrejudiceRemover, AdversarialDebiasing, MetaFairClassifier, GerryFairClassifier
+from fitness_rules import linear_parity, linear_odds, linear_opportunity
 from sklearn.preprocessing import StandardScaler
+import tensorflow.compat.v1 as tf_old
+tf_old.disable_eager_execution()
+import tensorflow as tf
 import numpy as np
 import pygad
 
@@ -53,6 +57,92 @@ def eval_metrics(model, dataset, unprivileged_groups, privileged_groups):
 
     return metrics
 
+
+def evolve_model(dataset_reader, model_initializer, fitness_rule):
+    dataset_expanded_train, dataset_train, dataset_val, dataset_test, unprivileged_groups, privileged_groups, sens_attr = dataset_reader()
+
+    def callback_generation(ga_instance):
+        print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+        print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
+
+    def fitness_function(solution, solution_idx):
+        # training
+        model = model_initializer(sens_attr, unprivileged_groups, privileged_groups, solution)
+
+        scaler = StandardScaler()
+
+        scaled_train = dataset_train.copy()
+        scaled_train.features = scaler.fit_transform(scaled_train.features)
+
+        scaled_val = dataset_val.copy()
+        scaled_val.features = scaler.transform(scaled_val.features)
+
+        model = model.fit(scaled_train)
+
+
+        val_metrics = eval_metrics(model, scaled_val, unprivileged_groups, privileged_groups)
+        fitness = fitness_rule(val_metrics)
+        print('-----------------------------------')
+        print('Solution %d' % solution_idx)
+        print('Solution:', str(solution))
+        print('Fitness:', fitness)
+        describe_metrics(val_metrics)
+        print('-----------------------------------')
+        return fitness
+
+    if fitness_rule is not None:
+        # best solution
+        gene_type = genes[model_initializer.__name__]['type']
+        gene_space = genes[model_initializer.__name__]['space']
+        ga_instance = pygad.GA(num_generations=num_generations,
+                               num_parents_mating=num_parents_mating,
+                               fitness_func=fitness_function,
+                               num_genes=len(gene_type),
+                               gene_space=gene_space,
+                               gene_type=gene_type,
+                               # initial_population=initial_population,
+                               sol_per_pop=sol_per_pop,
+                               parent_selection_type=parent_selection_type,
+                               crossover_type=crossover_type,
+                               crossover_probability=crossover_probability,
+                               mutation_type=mutation_type,
+                               mutation_probability=mutation_probability,
+                               keep_parents=keep_parents,
+                               on_generation=callback_generation)
+        ga_instance.run()
+        best_solution, best_solution_fitness, best_solution_idx = ga_instance.best_solution()
+        print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=best_solution_fitness))
+        print("Index of the best solution : {solution_idx}".format(solution_idx=best_solution_idx))
+
+        # eval on test set
+        model = model_initializer(sens_attr, unprivileged_groups, privileged_groups, best_solution)
+    else:
+        model = model_initializer(sens_attr, unprivileged_groups, privileged_groups)
+
+    scaler = StandardScaler()
+    scaled_expanded_train = dataset_expanded_train.copy()
+
+    scaled_expanded_train.features = scaler.fit_transform(scaled_expanded_train.features)
+
+    model = model.fit(scaled_expanded_train)
+
+    scaled_test = dataset_test.copy()
+    scaled_test.features = scaler.transform(scaled_test.features)
+
+    test_metrics = eval_metrics(model, scaled_test, unprivileged_groups, privileged_groups)
+
+    if fitness_rule is not None:
+        test_metrics['fitness_rule'] = fitness_rule.__name__
+    else:
+        test_metrics['fitness_rule'] = 'No optimization'
+
+    print('-----------------------------------')
+    describe_metrics(test_metrics)
+    test_metrics['method'] = model_initializer.__name__
+    print('-----------------------------------')
+
+    return test_metrics
+
 def describe_dataset(train=None, val=None, test=None):
     if train is not None:
         print("#### Training Dataset shape")
@@ -87,10 +177,11 @@ def adult_dataset_reader():
                                             'occupation', 'relationship', 'native-country', 'race'],
                       privileged_classes=[['Male']], metadata={'label_map': label_map,
                                                                'protected_attribute_maps': protected_attribute_maps})
-    (dataset_train,
-     dataset_val,
-     dataset_test) = AdultDataset().split([0.5, 0.8], shuffle=True)
+    (dataset_expanded_train,
+     dataset_test) = AdultDataset().split([0.8], shuffle=True)
 
+    (dataset_train,
+    dataset_val) = dataset_expanded_train.split([0.8], shuffle=True)
     sens_ind = 1
     sens_attr = dataset_train.protected_attribute_names[sens_ind]
 
@@ -101,9 +192,16 @@ def adult_dataset_reader():
 
     describe_dataset(dataset_train, dataset_val, dataset_test)
 
-    return dataset_train, dataset_val, dataset_test, unprivileged_groups, privileged_groups, sens_attr
+    return dataset_expanded_train, dataset_train, dataset_val, dataset_test, unprivileged_groups, privileged_groups, sens_attr
 
-def fair_mlp_initializer(sens_attr, solution=None):
+def gerry_fair_classifier_initializer(sens_attr, unprivileged_groups, privileged_groups, solution=None):
+    if solution is not None:
+        model = GerryFairClassifier(C=solution[0], gamma=solution[1], fairness_def='FP')
+    else:
+        model = GerryFairClassifier(fairness_def='FP')
+    return model
+
+def fair_mlp_initializer(sens_attr, unprivileged_groups, privileged_groups, solution=None):
     if solution is not None:
         model = FairMLP(sensitive_attr=sens_attr,
                         hidden_sizes=[16, 32],
@@ -116,128 +214,109 @@ def fair_mlp_initializer(sens_attr, solution=None):
                         batch_size=32)
     return model
 
-def prejudice_remover_initializer(sens_attr, solution=None):
+def simple_mlp_initializer(sens_attr, unprivileged_groups, privileged_groups, solution=None):
+    if solution is not None:
+        model = SimpleMLP(sensitive_attr=sens_attr,
+                        hidden_sizes=[16, 32],
+                        batch_size=32)
+    else:
+        model = SimpleMLP(sensitive_attr=sens_attr,
+                        hidden_sizes=[16, 32],
+                        batch_size=32)
+    return model
+
+def prejudice_remover_initializer(sens_attr, unprivileged_groups, privileged_groups, solution=None):
     if solution is not None:
         model = PrejudiceRemover(eta=solution[0], sensitive_attr=sens_attr)
     else:
         model = PrejudiceRemover(sensitive_attr=sens_attr)
     return model
 
-def meta_fair_classifier_sr_initializer(sens_attr, solution=None):
+def meta_fair_classifier_sr_initializer(sens_attr, unprivileged_groups, privileged_groups, solution=None):
     if solution is not None:
         model = MetaFairClassifier(tau=solution[0], sensitive_attr=sens_attr, type='sr')
     else:
         model = MetaFairClassifier(sensitive_attr=sens_attr, type='sr')
     return model
 
-def evolve_model(dataset_reader, model_initializer, fitness_rule):
-    dataset_train, dataset_val, dataset_test, unprivileged_groups, privileged_groups, sens_attr = dataset_reader()
-
-    def callback_generation(ga_instance):
-        print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-        print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-
-    def fitness_function(solution, solution_idx):
-        # training
-
-        model = model_initializer(sens_attr, solution)
-
-        scaler = StandardScaler()
-
-        scaled_train = dataset_train.copy()
-        scaled_train.features = scaler.fit_transform(scaled_train.features)
-
-        scaled_val = dataset_val.copy()
-        scaled_val.features = scaler.transform(scaled_val.features)
-
-        model = model.fit(scaled_train)
-
-
-        val_metrics = eval_metrics(model, scaled_val, unprivileged_groups, privileged_groups)
-        fitness = fitness_rule(val_metrics)
-        print('-----------------------------------')
-        print('Solution %d' % solution_idx)
-        print('Solution:', str(solution))
-        print('Fitness:', fitness)
-        describe_metrics(val_metrics)
-        print('-----------------------------------')
-        return fitness
-
-    if fitness_rule is not None:
-        # best solution
-        num_genes, init_range_low, init_range_high = genes[model_initializer.__name__]
-        ga_instance = pygad.GA(num_generations=num_generations,
-                               num_parents_mating=num_parents_mating,
-                               fitness_func=fitness_function,
-                               num_genes=num_genes,
-                               # initial_population=initial_population,
-                               sol_per_pop=sol_per_pop,
-                               init_range_low=init_range_low,
-                               init_range_high=init_range_high,
-                               parent_selection_type=parent_selection_type,
-                               crossover_type=crossover_type,
-                               crossover_probability=crossover_probability,
-                               mutation_type=mutation_type,
-                               mutation_probability=mutation_probability,
-                               keep_parents=keep_parents,
-                               on_generation=callback_generation)
-        ga_instance.run()
-        best_solution, best_solution_fitness, best_solution_idx = ga_instance.best_solution()
-        print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=best_solution_fitness))
-        print("Index of the best solution : {solution_idx}".format(solution_idx=best_solution_idx))
-
-        # eval on test set
-        model = model_initializer(sens_attr, best_solution)
+def meta_fair_classifier_fdr_initializer(sens_attr, unprivileged_groups, privileged_groups, solution=None):
+    if solution is not None:
+        model = MetaFairClassifier(tau=solution[0], sensitive_attr=sens_attr, type='fdr')
     else:
-        model = model_initializer(sens_attr)
+        model = MetaFairClassifier(sensitive_attr=sens_attr, type='fdr')
+    return model
 
-    scaler = StandardScaler()
-    scaled_train = dataset_train.copy()
-    scaled_train.features = scaler.fit_transform(scaled_train.features)
 
-    scaled_val = dataset_val.copy()
-    scaled_val.features = scaler.fit_transform(scaled_val.features)
 
-    model = model.fit(scaled_train)
-    model = model.fit(scaled_val)
-
-    scaled_test = dataset_test.copy()
-    scaled_test.features = scaler.transform(scaled_test.features)
-
-    test_metrics = eval_metrics(model, scaled_test, unprivileged_groups, privileged_groups)
-
-    if fitness_rule is not None:
-        test_metrics['fitness_rule'] = fitness_rule.__name__
+def adversarial_debiasing_initializer(sens_attr, unprivileged_groups, privileged_groups, solution=None):
+    if solution is not None:
+        model = AdversarialDebiasing(unprivileged_groups, privileged_groups,
+                                     'adv_debias_' + str(datetime.now().timestamp()), tf_old.Session(),
+                                     classifier_num_hidden_units=solution[0], adversary_loss_weight=solution[1])
     else:
-        test_metrics['fitness_rule'] = 'No optimization'
+        model = AdversarialDebiasing(unprivileged_groups, privileged_groups,
+                                     'adv_debias_' + str(datetime.now().timestamp()), tf_old.Session())
+    return model
 
-    print('-----------------------------------')
-    describe_metrics(test_metrics)
-    test_metrics['method'] = model_initializer.__name__
-    print('-----------------------------------')
-
-    return test_metrics
 
 
 methods = [
-    meta_fair_classifier_sr_initializer
+    meta_fair_classifier_fdr_initializer,
+    prejudice_remover_initializer,
+    fair_mlp_initializer,
 ]
 
 genes = {
-    'fair_mlp_initializer': (4, 0.0, 1.0),
-    'prejudice_remover_initializer': (1, 0.01, 50.0),
-    'meta_fair_classifier_sr_initializer' : (1, 0.01, 2.0),
-    'adversarial_debiasing_initializer'
+    'fair_mlp_initializer':  {
+                'type': [float, float, float, float],
+                'space': [
+                    {'low': 0.0, 'high': 1.0},
+                    {'low': 0.0, 'high': 1.0},
+                    {'low': 0.0, 'high': 1.0},
+                    {'low': 0.0, 'high': 1.0}
+                ]
+            },
+    'prejudice_remover_initializer':  {
+                'type': [float],
+                'space': [
+                    {'low': 0.0, 'high': 50.0}
+                ]
+            },
+    'meta_fair_classifier_sr_initializer': {
+                'type': [float],
+                'space': [
+                    {'low': 0.0, 'high': 2.0}
+                ]
+            },
+    'meta_fair_classifier_fdr_initializer': {
+                'type': [float],
+                'space': [
+                    {'low': 0.0, 'high': 2.0}
+                ]
+            },
+    'adversarial_debiasing_initializer': {
+                'type': [int, float],
+                'space': [
+                    [32, 64, 128, 256],
+                    {'low': 0.0, 'high': 1.0}
+                ]
+            },
+    'gerry_fair_classifier_initializer': {
+                'type': [float, float],
+                'space': [
+                    {'low': 0.0, 'high': 20.0},
+                    [0.1, 0.01, 0.001]
+                ]
+            },
 }
 
 datasets = [
-    adult_dataset_reader
+    adult_dataset_reader,
+
 ]
 
 rules = [
-    None,
-    paraboloid_parity,
-    linear_parity
+    linear_opportunity
 ]
 
 results = []
@@ -255,4 +334,4 @@ for dataset_reader in datasets:
             describe_metrics(best_metrics)
             results.append(best_metrics)
             results_df = pd.DataFrame(results)
-            results_df.to_csv('results2.csv')
+            results_df.to_csv('odds_results.csv')
---
+++ notebooks/2022-05-03/replot.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_comparison(results, dataset_name, fairness_metric):
+    my_cmap = plt.cm.get_cmap('tab20')
+    n_bars = len(results.keys()) - 1
+    colors = my_cmap([x for x in range(20)])
+    print(results)
+    fig, axis = plt.subplots(3, 1, figsize=(14, 12), sharex=True,
+                             gridspec_kw={'height_ratios': [0.5, 2, 2]})
+    # fig.suptitle("Results on %s Dataset minimizing %s\n" % (dataset_name, fairness_metric), fontsize=16)
+    axis[1].tick_params(axis='x', which='major', labelsize=20)
+    axis[1].tick_params(axis='y', which='major', labelsize=20)
+    axis[2].tick_params(axis='x', which='major', labelsize=20)
+    axis[2].tick_params(axis='y', which='major', labelsize=20)
+    axis[0].axis('off')
+    axis[1] = results[['overall_acc', 'bal_acc', 'method']] \
+        .plot.bar(x='method',rot=0, ax=axis[1], legend=False, color=colors[:2])
+    axis[2] = results[['avg_odds_diff', 'stat_par_diff', 'eq_opp_diff', 'method']] \
+        .plot.bar(x='method', rot=0, ax=axis[2], legend=False, color=colors[[4,6,8]])
+    axis[1].set_ylim(0, 0.9)
+    axis[2].set_ylim(0, 0.4)
+    axis[2].set_xlabel('Method', fontsize=24)
+    axis[1].xaxis.labelpad = 40
+    axis[2].xaxis.labelpad = 20
+    axis[1].grid(visible=True, which='major', axis='y', color='black', linewidth=1)
+    axis[2].grid(visible=True, which='major', axis='y', color='black', linewidth=1)
+
+    bars_labels = [ax.get_legend_handles_labels() for ax in fig.axes]
+    bars, labels = [sum(lol, []) for lol in zip(*bars_labels)]
+    metric_explain = get_metric_explain()
+    labels = [metric_explain[label] for label in labels]
+    fig.legend(bars, labels,
+               loc='upper center',
+               fancybox=True, shadow=False, ncol=2,
+               fontsize='xx-large')
+    fig.tight_layout()
+    file_name = '%s_%s_plot.png' % (dataset_name, fairness_metric)
+    file_name = file_name.lower().replace(' ', '_')
+
+    fig.savefig(file_name, dpi=300)
+
+def get_metric_explain():
+    return {
+        'overall_acc': 'Accuracy',
+        'bal_acc': 'Balanced Accuracy',
+        'avg_odds_diff': 'Equalized Odds',
+        'disp_imp': 'Disparate Impact',
+        'stat_par_diff': 'Statistical Parity',
+        'eq_opp_diff': 'Equal Opportunity'
+    }
+
+results = pd.read_csv('parity_results.csv')
+plot_comparison(results, 'Income', 'Statistical Parity')
+
+results = pd.read_csv('odds_results.csv')
+plot_comparison(results, 'Income', 'Equalized Odds')
+
+
