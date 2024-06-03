f2de3016d65931ac314e46b17979d935d8858f06
Ygor Canalli <ygor.canalli@gmail.com>
2024-06-02 23:29:44 -0300

    Cleaning unused files, organizing KNOSYS results

--- fitness_rules.py
+++ fitness_rules.py
@@ -1,20 +1,3 @@
-import numpy as np
-
-def paraboloid_parity(metrics):
-    acc = metrics['overall_acc']
-    par = metrics['stat_par_diff']
-    return -par**2 - (acc-1)**2
-
-def paraboloid_odds(metrics):
-    acc = metrics['overall_acc']
-    odds = metrics['avg_odds_diff']
-    return -odds**2 - (acc-1)**2
-
-def paraboloid_opportunity(metrics):
-    acc = metrics['overall_acc']
-    opp = metrics['eq_opp_diff']
-    return -opp**2 - (acc-1)**2
-
 def acc_parity(metrics):
     acc = metrics['overall_acc']
     par = metrics['stat_par_diff']
@@ -44,28 +27,3 @@ def mcc_opportunity(metrics):
     mcc = metrics['MCC']
     opp = metrics['eq_opp_diff']
     return mcc - opp
-
-def bal_acc_linear_parity(metrics):
-    acc = metrics['bal_acc']
-    par = metrics['stat_par_diff']
-    return acc - abs(par)
-
-def bal_acc_linear_odds(metrics):
-    acc = metrics['bal_acc']
-    odds = metrics['avg_odds_diff']
-    return acc - abs(odds)
-
-def bal_acc_linear_opportunity(metrics):
-    acc = metrics['bal_acc']
-    opp = metrics['eq_opp_diff']
-    return acc - abs(opp)
-def linear_all(metrics):
-    acc = metrics['overall_acc']
-    opp = metrics['eq_opp_diff']
-    odds = metrics['avg_odds_diff']
-    par = metrics['stat_par_diff']
-    unfairness = np.sqrt(opp**2 + odds**2 + par**2)
-    return acc - unfairness
-
-def accuracy_only(metrics):
-    return metrics['overall_acc']
